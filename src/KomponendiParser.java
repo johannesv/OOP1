e3fd1b9ce393e6135d4547bfbaeb010e2776a618
Johannes Vainikko <johannes.vainikko@gmail.com>
2013-03-13 21:15:46 +0200

    Added part files and also method for copying parts file when there are non present

--- src/KomponendiParser.java
+++ src/KomponendiParser.java
@@ -1,7 +1,12 @@
 import java.io.*;
+import java.util.Scanner;
+import static java.nio.file.StandardCopyOption.*;
+
 
 public class KomponendiParser {
 	public static void main(String args[]) {
+		//failiteed();
+		osadeKontroll();
 		Komponent komp;
 		try{
 			  // Open the file that is the first 
@@ -16,7 +21,7 @@ public class KomponendiParser {
 			  // Print the content on the console
 				  if (strLine.length()>= 2) {
 					  if (!strLine.substring(0,2).equals("//")) {
-						  System.out.println (strLine);
+						  //System.out.println (strLine);
 						  if (strLine.substring(0,6).equals("name = ")) {
 							  //komp = new Komponent();
 							  //komp.nimi = strLine.substring(7,0);
@@ -32,5 +37,80 @@ public class KomponendiParser {
 			  System.err.println("Error: " + e.getMessage());
 			  }
 			  
+	}
+	
+	public static String[] failiteed() {
+		File[] failid = new File("D:/KSP_win/Parts").listFiles();
+	    //showFiles(files);
+		String[] osad = new String[2];
+		for (File fail : failid) {
+			//System.out.println(fail);
+			
+		}
+		return osad;
+	}
+	
+	
+	// On eeldatud et programm käivitatakse kaustast kuhu on paigaldatud Kerbal Space Program.
+	// Kui KSP-osade faile ei leitud, siis küsida KSP kataloogi asukohta ja sealt osade failid programmi kausta kopeerida
+	public static void osadeKontroll() {
+		File osad = new File("Parts");
+		if (osad.isDirectory()) return;
+		
+		 //Scanner scan = new Scanner(System.in);
+
+		  //System.out.println("Sisesta KSP baaskausta aukoht(D:/KSP_win):");
+
+		  //String asukoht = scan.next();
+		  
+		  //osad = new File(asukoht + "/Parts");
+			osad = new File("D:/KSP_win/Parts");
+			
+		  if (osad.isDirectory()) {
+			  String asukoht = "Parts";
+			  File uuedOsad = new File(asukoht);
+			  uuedOsad.mkdir();
+			  
+			  for (File kaust : osad.listFiles()) {
+				  
+				  File uusOsa = new File(asukoht + "/" + kaust.getName());
+				  uusOsa.mkdir();
+				  
+				  File uusFail = new File(uusOsa.getPath()+"/part.cfg");
+				  
+				  File partFail = new File(kaust.getPath()+"/part.cfg");
+				  
+				  
+				  //System.out.println(partFail);
+				  
+				  
+				  //copyFileToDirectory(partFail, uusOsa);
+				  
+				  //FileUtils.copyFile();
+				  
+				  try {
+					InputStream sisse = new FileInputStream(partFail);
+					OutputStream valja = new FileOutputStream(uusFail);
+					
+					byte[] buf = new byte[1024];
+					  int len;
+					  while ((len = sisse.read(buf)) > 0){
+					  valja.write(buf, 0, len);
+					  }
+					  sisse.close();
+					  valja.close();
+					
+				} catch(FileNotFoundException ex){
+					  System.out.println(ex.getMessage() + " in the specified directory.");
+					  System.exit(0);
+					  }
+					  catch(IOException e){
+					  System.out.println(e.getMessage());  
+				} 
+				  
+			  }
+			  
+		  }
+		//System.out.println("Exists " + osad.isDirectory());
 	}
 }
